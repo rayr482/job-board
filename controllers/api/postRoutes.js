44a998db44bd2a5b69a1f6d35c9236f8b4953e46
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2023-07-17 21:23:08 -0700

    postRoutes created

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,84 @@
+const router = require('express').Router();
+const { Category, Post, User } = require('../../models');
+
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: Category,
+          attributes: ['category_name']
+        },
+        {
+          model: User,
+          attributes: ['name']
+        }
+      ]
+    });
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const postData = await Post.findbypk(req.params.id, {
+        include: [
+            {
+              model: Category,
+              attributes: ['category_name']
+            },
+            {
+              model: User,
+              attributes: ['name']
+            }
+          ]
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: "No post found with this ID." });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+    try {
+      const newPost = await Post.create({
+        ...req.body,
+        user_id: req.session.user_id,
+      });
+  
+      res.status(200).json(newPost);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+  
+  router.delete('/:id', async (req, res) => {
+    try {
+      const postData = await Post.destroy({
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      });
+  
+      if (!postData) {
+        res.status(404).json({ message: 'No post found with this ID.' });
+        return;
+      }
+  
+      res.status(200).json(postData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+module.exports = router;
