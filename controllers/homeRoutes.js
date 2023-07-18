16abd3e8b1dd806dc89f7873220a9e7b1f8e7542
kristinehkim <122055613+kristinehkim@users.noreply.github.com>
2023-07-18 16:18:11 -0700

    added withAuth and seed files

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,15 +1,59 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const { User, Post } = require("../models");
+const withAuth = require("../utils/auth");
 
-router.get('/', async (req, res) => {
-  res.render('home', {
-    layout: 'main'
-  });
+router.get('/posts', async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const posts = postData.map((project) => project.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('all-posts', { 
+      layout: 'main',
+      posts, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+// Use withAuth middleware to prevent access to route
+router.get("/", withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Post }],
+    });
+    const user = userData.get({ plain: true });
+
+    res.render('home', {
+      // layout: 'main'
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/login', async (req, res) => {
-  res.render('login', {
-    layout: 'main'
-  });
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+    }
+    res.render('login');
 });
 
 module.exports = router;
