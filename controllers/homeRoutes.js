1a7322df663151a56b6bc27f15416c2078791e43
kristinehkim <122055613+kristinehkim@users.noreply.github.com>
2023-07-18 19:17:38 -0700

    getting posts and added seed files

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -16,7 +16,7 @@ router.get('/posts', async (req, res) => {
 
     // Serialize data so the template can read it
     const posts = postData.map((project) => project.get({ plain: true }));
-
+console.log(posts);
     // Pass serialized data and session flag into template
     res.render('all-posts', { 
       layout: 'main',
