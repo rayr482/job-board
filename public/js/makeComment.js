2eae4849272bfeb3dc8e54fd0c9eb5d659a69e36
rayr482 <rar002@ucsd.edu>
2023-07-22 22:32:16 -0700

    comment not longer needs user name

--- public/js/makeComment.js
+++ public/js/makeComment.js
@@ -2,7 +2,7 @@ const newCommentHandler = async (event) => {
   event.preventDefault();
 
   const content = document.querySelector("#comment-content").value.trim();
-  const user = document.querySelector("#comment-user").value.trim();
+//   const user = document.querySelector("#comment-user").value.trim();
   const urlParams = new URLSearchParams(window.location.search);
   const postId = urlParams.get('postId');
 
@@ -12,10 +12,10 @@ const newCommentHandler = async (event) => {
   // console.log("Post ID:", postId);
 
 
-  if (content && user && postId) {
+  if (content && postId) {
     const response = await fetch('/api/comments', {
       method: "POST",
-      body: JSON.stringify({ content, user, post_id: postId }),
+      body: JSON.stringify({ content, post_id: postId }),
       headers: {
         "Content-Type": "application/json",
       },
