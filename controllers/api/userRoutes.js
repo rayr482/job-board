0c2e3b0abb2e808a6ac69425d0f5aec364fe8858
rayr482 <rar002@ucsd.edu>
2023-07-23 14:06:43 -0700

    set up post route for nodemailer

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -42,6 +42,8 @@ router.post('/login', async (req, res) => {
     }
     req.session.save(() => {
       req.session.user_id = userData.id;
+      req.session.name = userData.name;
+      req.session.email = userData.email;
       req.session.logged_in = true;
       res.json({ user: userData, message: 'You are now logged in!' });
     });
